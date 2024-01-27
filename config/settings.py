b952ec54b73b74fee7291e01b4114b35676dfcf9
IPoorya <brabbitcontact@gmail.com>
2024-01-27 22:34:55 +0330

    v1.0

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*ik*yaepoi%-!izqe-i52yviukxoh(rfy4xrb1+pj_*#iurh&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
