2e8170f5bd69468c8830c4fd1d24fffe53efa573
IPoorya <brabbitcontact@gmail.com>
2024-01-28 01:04:33 +0330

    swagger url changed

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
      # YOUR PATTERNS
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
